dc39ef16ee9b5cba7ad5732e61c79fb16820c556
salva00786 <salva00786@gmail.com>
2025-05-27 13:45:26 +0200

    Add files via upload

---
+++ android/app/src/main/java/com/example/jetpin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jetpin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 
@@ -12,7 +11,7 @@ class JetPinApp extends StatefulWidget {
   const JetPinApp({Key? key}) : super(key: key);
 
   @override
-  _JetPinAppState createState() => _JetPinAppState();
+  State<JetPinApp> createState() => _JetPinAppState();
 }
 
 class _JetPinAppState extends State<JetPinApp> {
@@ -37,22 +36,15 @@ class _JetPinAppState extends State<JetPinApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'JetPin',
+      supportedLocales: const [Locale('en'), Locale('it')],
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('it', ''),
-      ],
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(localizedStrings['title'] ?? 'JetPin'),
-        ),
-        body: Center(
-          child: Text(localizedStrings['welcome'] ?? 'Welcome'),
-        ),
+        appBar: AppBar(title: Text(localizedStrings['title'] ?? 'JetPin')),
+        body: Center(child: Text(localizedStrings['welcome'] ?? 'Welcome')),
       ),
     );
   }

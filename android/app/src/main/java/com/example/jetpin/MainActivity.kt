238efd667460a9292186d35243f89b16e5f81485
salva00786 <salva00786@gmail.com>
2025-05-27 15:10:00 +0200

    Delete android directory

--- android/app/src/main/java/com/example/jetpin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jetpin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/jetpin_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jetpin_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
